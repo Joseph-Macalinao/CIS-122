ce5c6f3fc851adba55a84131982182a0eb0ea4b6
Joseph Macalinao <jhmac121101@gmail.com>
2021-05-07 17:33:20 -0700

    test

--- cis122-assign05-guess.py
+++ cis122-assign05-guess.py
@@ -4,36 +4,69 @@ Author: Jospeh Macalinao
 Credit: Me and Kaya Jang
 Description: Make a program that fills in missing letters in a word
 '''
-
-
-
 def guess():
-    guessed_word_complete = False
+    '''
+    This guess game will ask for a word then have the user guess letters
+
+    This function will ask the user for a word, then will prompt the user for a repeated guess /
+    for letters in the word. The function will also tell the user if the letter is not found /
+    and if or the lette has already been guessed
+
+    arg - none
+
+    return - n/a
+    '''
     word = input("Enter a guess word (blank to quit):")
+    if word == '':
+        return 0
     guess_bank = ''
+    matched_guess_bank = ''
     count = 0
-    while guessed_word_complete == False:
-        guess_letter =input("Enter a letter you would like to guess: ")
+    len_list = len(word)
+    len_guess = (1 * len_list)
+    #while loop that will continually go through guesses until all letters are found
+    while len_guess > 0:
+        guess_letter = input("Enter a letter you would like to guess: ")
+        #checking to make sure that the input letter is only 1 letter
         if len(guess_letter) > 1:
-            print("Letter must only be 1 character.")
-            guess_letter =input("Enter a letter you would like to guess:")
+            print("Only enter a single letter.")
+            count += 1
+            guess_letter = input("Enter a letter you would like to guess (blank to quit):")
+        elif guess_letter == '':
+            return 0
+        if search(guess_bank,guess_letter) == True:
+            print("That letter has already been guessed.")
+            guess_letter = input("Enter a letter you would like to guess.")
+
+        if guess_letter not in word:
+            print("Letter not found:")
+            guess_letter = input("Enter a letter you would like to guess.")
+            count += 1
         for letter in word:
+
             if guess_letter == letter:
                 letter = guess_letter
+                len_guess -= 1
+                matched_guess_bank = matched_guess_bank + letter
             else:
                 letter = 0
-            print(letter, end = '')
+            print(letter, end='')
+        print()
         guess_bank = guess_bank + guess_letter
-        print()
         print(f'Guesses so far: {guess_bank}')
-        print()
         count += 1
         print(f'Guesses: {count}')
+    print("*** Results ***")
+    print(f'Word: \t{word}')
+    print(f'Guessed: \t{guess_bank}')
+    print(f'Matched: \t{matched_guess_bank}')
+    print(f'Guesses: {count}')
 
-def search(string):
-    count_0 = 0
-    for letter in string:
-        if letter == '0':
-            count_0 += 1
-    return  count_0
+def search(string, letter):
+    for char in string:
+        if char == letter:
+            return True
+        else:
+            return False
+
 guess()
--- messing.py
+++ messing.py
@@ -5,10 +5,22 @@ Credit: Me and Kaya Jang
 Description: Make a program that fills in missing letters in a word
 '''
 def guess():
+    '''
+    This guess game will ask for a word then have the user guess letters
+
+    This function will ask the user for a word, then will prompt the user for a repeated guess /
+    for letters in the word. The function will also tell the user if the letter is not found /
+    and if or the lette has already been guessed
+
+    arg - none
+
+    return - n/a
+    '''
     word = input("Enter a guess word (blank to quit):")
     if word == '':
         return 0
     guess_bank = ''
+    matched_guess_bank = ''
     count = 0
     len_list = len(word)
     len_guess = (1 * len_list)
@@ -17,21 +29,24 @@ def guess():
         guess_letter = input("Enter a letter you would like to guess: ")
         #checking to make sure that the input letter is only 1 letter
         if len(guess_letter) > 1:
-            print("Letter must only be 1 character.")
+            print("Only enter a single letter.")
+            count += 1
             guess_letter = input("Enter a letter you would like to guess (blank to quit):")
         elif guess_letter == '':
             return 0
-        if search(guess_bank,guess_letter):
+        if search(guess_bank,guess_letter) == True:
             print("That letter has already been guessed.")
             guess_letter = input("Enter a letter you would like to guess.")
+
+        if guess_letter not in word:
+            print("Letter not found:")
+            guess_letter = input("Enter a letter you would like to guess.")
         for letter in word:
-            if search(word, letter) == False:
-                print("That letter wasn't found.")
-                guess_letter = input("Enter a letter you would like to guess")
+
             if guess_letter == letter:
                 letter = guess_letter
                 len_guess -= 1
-
+                matched_guess_bank = matched_guess_bank + letter
             else:
                 letter = 0
             print(letter, end='')
@@ -40,12 +55,15 @@ def guess():
         print(f'Guesses so far: {guess_bank}')
         count += 1
         print(f'Guesses: {count}')
-    print(f'Good job, you guessed the word: {word} in {count} tries.')
-    print(f'You matched: {guess_bank}')
+    print("*** Results ***")
+    print(f'Word: \t{word}')
+    print(f'Guessed: \t{guess_bank}')
+    print(f'Matched: \t{matched_guess_bank}')
+    print(f'Guesses: {count}')
 
 def search(string, letter):
-    for i in string:
-        if letter == letter:
+    for char in string:
+        if char == letter:
             return True
         else:
             return False
