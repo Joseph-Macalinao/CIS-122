7a82f07e022d38fb66dd97c310dc5b53ecf09337
Joseph Macalinao <jhmac121101@gmail.com>
2021-05-11 17:28:08 -0700

    test

---
+++ Assignment 6/cis122-assign06-random.py
@@ -0,0 +1,22 @@
+def file():
+    na = open('random_numbers.txt')
+    total_hash = 0
+    total_num = 0
+    total = 0
+    for line in na:
+        line.strip()
+        line.lower()
+        if line[0] == '#':
+            total_hash += 1
+        else:
+            line = int(line)
+            total_num += 1
+            total += line
+
+    avg = total / total_num
+    print(f'Count: {total_num}')
+    print(f'Comments: {total_hash}')
+    print(f'Total: {total}')
+    print(f'Average: {avg}')
+    na.close()
+file()
